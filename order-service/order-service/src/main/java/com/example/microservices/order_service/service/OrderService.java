ea5d85b9c07bc5798e9643542d5c4c9403d7508e
DhruvShah <dhruvshah78223@gmail.com>
2024-08-10 13:29:58 +0530

    inverntory-service related microservices are added.

--- order-service/order-service/src/main/java/com/example/microservices/order_service/service/OrderService.java
+++ order-service/order-service/src/main/java/com/example/microservices/order_service/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.example.microservices.order_service.service;
 
+import com.example.microservices.order_service.client.InventoryClient;
 import com.example.microservices.order_service.dto.OrderRequest;
 import com.example.microservices.order_service.model.Order;
 import com.example.microservices.order_service.repository.OrderRepository;
@@ -14,15 +15,23 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-
+    private final InventoryClient inventoryClient;
 
     public void placeOrder(OrderRequest orderRequest) {
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price().multiply(BigDecimal.valueOf(orderRequest.quantity())));
-        order.setSkuCode(orderRequest.skuCode());
-        order.setQuantity(orderRequest.quantity());
-        orderRepository.save(order);
+      var isInProduct=inventoryClient.isInStock(orderRequest.skuCode(),orderRequest.quantity());
+
+      if(isInProduct){
+          Order order = new Order();
+          order.setOrderNumber(UUID.randomUUID().toString());
+          order.setPrice(orderRequest.price().multiply(BigDecimal.valueOf(orderRequest.quantity())));
+          order.setSkuCode(orderRequest.skuCode());
+          order.setQuantity(orderRequest.quantity());
+          orderRepository.save(order);
+      }
+      else{
+          throw new RuntimeException("Product with Skucode"+orderRequest.skuCode()+"is not in stock");
+      }
+
     }
 
 }
