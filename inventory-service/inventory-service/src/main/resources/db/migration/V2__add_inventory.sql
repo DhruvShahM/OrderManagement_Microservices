4efa8c4cca91b0dea7a77ea36512f884af0f6ac4
DhruvShah <dhruvshah78223@gmail.com>
2024-08-14 11:11:52 +0530

    Removed all files for empty branch

---
+++ inventory-service/inventory-service/src/main/resources/db/migration/V2__add_inventory.sql
@@ -0,0 +1,5 @@
+INSERT INTO t_inventory (quantity, sku_code) VALUES
+(108, 'iphone_15'),
+(100, 'pixel_8'),
+(100, 'galaxy_24'),
+(180, 'oneplus_12');
---
+++ order-service/src/main/java/com/example/microservices/order_service/dto/OrderRequest.java
@@ -0,0 +1,12 @@
+package com.example.microservices.order_service.dto;
+
+import io.swagger.v3.oas.annotations.info.Contact;
+
+import java.math.BigDecimal;
+
+public record OrderRequest(Long id, String orderNumber, String skuCode,
+                           BigDecimal price, Integer quantity,UserDetails userDetails) {
+
+
+    public record UserDetails(String email, String firstName, String lastName) {}
+}
